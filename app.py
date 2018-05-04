97a6c0ab29339bec301353d5481873d5b19834aa
Ben Hasselgren <ben.nadalito@gmail.com>
2018-05-04 14:14:36 +0000

    Added base html and links and scripts to set up materualize

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb://admin:Ev00bish3!@ds115420.mlab.com:15420/task_manager'
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", 
+    tasks=mongo.db.tasks.find())
+
+#@app.route('/')
+#def hello():
+#    return 'Hello World'
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
